b32536eeed73e60ca4de54b687e7a40791ea4d26
Kevin DeAngeles <kevindeangelesjr@gmail.com>
2020-06-11 15:48:10 -0400

    completed pset4 filter

--- c/psets/pset4/filter/helpers.c
+++ c/psets/pset4/filter/helpers.c
@@ -10,12 +10,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Calculate average of Blue, Green, Red values
-            float rgbAvg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            float rgbAvg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.00;
 
             // Set RGB values to average for all three to create grayscale
-            image[i][j].rgbtBlue = round(rgbAvg);
-            image[i][j].rgbtGreen = round(rgbAvg);
-            image[i][j].rgbtRed = round(rgbAvg);
+            image[i][j].rgbtBlue = roundf(rgbAvg);
+            image[i][j].rgbtGreen = roundf(rgbAvg);
+            image[i][j].rgbtRed = roundf(rgbAvg);
         }
     }
 
@@ -30,19 +30,19 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Calculate RGB values for sepia colors
-            int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            float sepiaRed = roundf(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
             if (sepiaRed > 255)
             {
                 sepiaRed = 255;
             }
 
-            int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            float sepiaGreen = roundf(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             if (sepiaGreen > 255)
             {
                 sepiaGreen = 255;
             }
 
-            int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            float sepiaBlue = roundf(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
             if (sepiaBlue > 255)
             {
                 sepiaBlue = 255;
@@ -106,88 +106,41 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Create variables needed to keep track of totals and create averages
-            int blueAvg = 0;
-            int blueSum = 0;
-            int greenAvg = 0;
-            int greenSum = 0;
-            int redAvg = 0;
-            int redSum = 0;
-            int totalNeighbors = 0;
+            float blueAvg = 0;
+            float blueSum = 0;
+            float greenAvg = 0;
+            float greenSum = 0;
+            float redAvg = 0;
+            float redSum = 0;
+            float totalNeighbors = 0;
 
-            // Add values for the pixel itself
-            blueSum += image[i][j].rgbtBlue;
-            greenSum += image[i][j].rgbtGreen;
-            redSum += image[i][j].rgbtRed;
-            totalNeighbors++;
-
-            // If pixel is NOT on a bottom edge
-            if (!(i - 1 < 0))
+            // Loop through surrounding pixels
+            for (int k = -1; k < 2; k++)
             {
-                blueSum += image[i - 1][j].rgbtBlue;
-                greenSum += image[i - 1][j].rgbtGreen;
-                redSum += image[i - 1][j].rgbtRed;
-                totalNeighbors++;
-
-                // If pixel is not on a right edge
-                if (!(j + 1 > width))
+                if (i + k < 0 || i + k >= height)
                 {
-                    blueSum += image[i - 1][j + 1].rgbtBlue;
-                    greenSum += image[i - 1][j + 1].rgbtGreen;
-                    redSum += image[i - 1][j + 1].rgbtRed;
-                    totalNeighbors++;
-
-                    blueSum += image[i][j + 1].rgbtBlue;
-                    greenSum += image[i][j + 1].rgbtGreen;
-                    redSum += image[i][j + 1].rgbtRed;
-                    totalNeighbors++;
+                    continue;
                 }
 
-                // If pixel is not on a left edge
-                if (!(j - 1 < 0))
+                for (int m = -1; m < 2; m++)
                 {
-                    blueSum += image[i - 1][j - 1].rgbtBlue;
-                    greenSum += image[i - 1][j - 1].rgbtGreen;
-                    redSum += image[i - 1][j - 1].rgbtRed;
-                    totalNeighbors++;
+                    if (j + m < 0 || j + m >= width)
+                    {
+                        continue;
+                    }
 
-                    blueSum += image[i][j - 1].rgbtBlue;
-                    greenSum += image[i][j - 1].rgbtGreen;
-                    redSum += image[i][j - 1].rgbtRed;
-                    totalNeighbors++;
+                    blueSum += image[i + k][j + m].rgbtBlue;
+                    greenSum += image[i + k][j + m].rgbtGreen;
+                    redSum += image[i + k][j + m].rgbtRed;
+
+                    totalNeighbors++;                    
                 }
             }
 
-            // If pixel is not on a top edge
-            if (!(i + 1 > height))
-            {
-                blueSum += image[i + 1][j].rgbtBlue;
-                greenSum += image[i + 1][j].rgbtGreen;
-                redSum += image[i + 1][j].rgbtRed;
-                totalNeighbors++;
-
-                // If the pixel is not on a right edge
-                if (!(j + 1 > width))
-                {
-                    blueSum += image[i + 1][j + 1].rgbtBlue;
-                    greenSum += image[i + 1][j + 1].rgbtGreen;
-                    redSum += image[i + 1][j + 1].rgbtRed;
-                    totalNeighbors++;
-                }
-
-                // If the pixel is not on a left edge
-                if (!(j - 1 < 0))
-                {
-                    blueSum += image[i + 1][j - 1].rgbtBlue;
-                    greenSum += image[i + 1][j - 1].rgbtGreen;
-                    redSum += image[i + 1][j - 1].rgbtRed;
-                    totalNeighbors++;
-                }
-            }
-        
             // Calculate average 
-            blueAvg = blueSum / totalNeighbors;
-            greenAvg = greenSum / totalNeighbors;
-            redAvg = redSum / totalNeighbors;
+            blueAvg = roundf(blueSum / totalNeighbors);
+            greenAvg = roundf(greenSum / totalNeighbors);
+            redAvg = roundf(redSum / totalNeighbors);
 
             // Set temporary RGBTRIPLE struct with blurred values
             blurImage[i][j].rgbtBlue = blueAvg;
