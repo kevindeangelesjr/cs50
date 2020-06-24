1eb12061c5adecd1306a97e369b6bc82cf1d234f
Kevin DeAngeles <kevindeangelesjr@gmail.com>
2020-06-24 15:02:44 -0400

    06-24-2020

--- sql/psets/pset7/houses/import.py
+++ sql/psets/pset7/houses/import.py
@@ -1,1 +1,7 @@
-# TODO
+import cs50
+import sys
+import csv
+
+### Exit and print usage if not all arguments are provided (or too many are provided)
+if len(sys.argv) != 2:
+    print("Usage: python import.py data.csv")
