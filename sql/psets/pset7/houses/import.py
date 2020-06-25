1baec2d47a5f45ee88e0c3aae70be2faf46b6dd0
Kevin DeAngeles <kevindeangelesjr@gmail.com>
2020-06-25 13:53:15 -0400

    completed pset7 houses

--- sql/psets/pset7/houses/import.py
+++ sql/psets/pset7/houses/import.py
@@ -5,3 +5,41 @@ import csv
 ### Exit and print usage if not all arguments are provided (or too many are provided)
 if len(sys.argv) != 2:
     print("Usage: python import.py data.csv")
+    exit(1)
+    
+### CSV file is provided - open file and create CSV reader for it
+else:
+    csv_file = open(sys.argv[1], "r")
+    csv_reader = csv.DictReader(csv_file)
+
+### Open the students DB
+db = cs50.SQL("sqlite:///students.db")
+
+### Loop through each student record in the CSV file
+for row in csv_reader:
+
+    ### Pull fields from CSV file for insertion into DB
+    birth = row["birth"]
+    house = row["house"]
+
+    ### Name field may contain middle name, split into list for parsing
+    name = row["name"].split()
+    
+    ### If the name only has a length of 2, set first and last names, but set middle name to NULL
+    if len(name) == 2:
+        first = name[0]
+        middle = None
+        last = name[1]
+
+    ### If the name has a length of 3, set first, middle, and last names
+    elif len(name) == 3:
+        first = name[0]
+        middle = name[1]
+        last = name[2]
+
+    ### Insert fields extracted from CSV into the DB
+    db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?,?,?,?,?)", first, middle, last, house, birth)
+
+### Close the CSV file and exit
+csv_file.close()
+exit(1)
--- sql/psets/pset7/houses/roster.py
+++ sql/psets/pset7/houses/roster.py
@@ -1,1 +1,36 @@
-# TODO
+import cs50
+import sys
+
+### Exit and print usage if not all arguments are provided (or too many are provided)
+if len(sys.argv) != 2:
+    print("Usage: python roster.py house")
+    exit(1)
+
+### If provided properly, convert house to title case for querying sake
+else:
+    house = sys.argv[1].title()
+
+### Open the students DB
+db = cs50.SQL("sqlite:///students.db")
+
+### Query the DB for names and birth for records where the house matches the provided house
+query_results=db.execute("SELECT first,middle,last,birth FROM students WHERE house=(?) ORDER BY last", house)
+
+### Loop through queries returned
+for result in query_results:
+
+    ### Set values with values from query result
+    birth = result['birth']
+    first = result['first']
+    middle = result['middle']
+    last = result['last']
+
+    ### If no middle name
+    if middle == None:
+        print(f"{first} {last}, born {birth}")
+    
+    ### If middle name
+    else:
+        print(f"{first} {middle} {last}, born {birth}")
+    
+exit(0)
