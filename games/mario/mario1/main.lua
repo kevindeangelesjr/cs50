05362f6c549eb0f9af5fdecd8a9912a78c938600
Kevin DeAngeles <kevindeangelesjr@gmail.com>
2020-07-18 14:00:53 -0400

    07-18-2020

---
+++ games/mario/mario1/main.lua
@@ -0,0 +1,47 @@
+-- Window settings
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- Import class and push libraries
+Class = require 'class'
+push = require 'push'
+
+-- Import Map class from Map.lua and import functions from Util.lua
+require 'Util'
+require 'Map'
+
+function love.load()
+
+    map = Map()
+
+    -- Remove blurriness
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- Set up screen
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.update(dt)
+
+    map:update(dt)
+
+end
+
+function love.draw()
+
+    push:apply('start')
+
+    love.graphics.translate(math.floor(-map.camX), math.floor(-map.camY))
+
+    -- Screen background color
+    love.graphics.clear(108 / 255, 140 / 255, 1 , 1)
+    map:render()
+
+    push:apply('end')
+end
